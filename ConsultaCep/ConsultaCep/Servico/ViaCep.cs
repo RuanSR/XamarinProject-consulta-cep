d8f23187ef14f92b6d5ce71fe417449d64c109a8
RuanSR <ruansilvarosa@gmail.com>
2018-09-06 00:08:58 -0300

    2 - Criado classes Endereco e ViaCep, ambas parcialmete implementadas

---
+++ ConsultaCep/ConsultaCep/Servico/ViaCep.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsultaCep.Servico
+{
+    public class ViaCep
+    {
+        private static string EnderecoURL = "https://viacep.com.br/ws/{0}/json/";
+    }
+}
