fa5ff5bbc95df5c314687a2cf415c25f3b8cc711
RuanSR <ruansilvarosa@gmail.com>
2018-09-06 00:48:30 -0300

    3 - Tela de interface do usuario implementada, imagem Logo.png importada.

--- ConsultaCep/ConsultaCep/Servico/ViaCep.cs
+++ ConsultaCep/ConsultaCep/Servico/ViaCep.cs
@@ -1,11 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Net;
+using Newtonsoft.Json;
 
 namespace ConsultaCep.Servico
 {
     public class ViaCep
     {
         private static string EnderecoURL = "https://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCep(string cep)
+        {
+            string novoEnderecoURL = string.Format(EnderecoURL, cep);
+
+            WebClient wc = new WebClient();
+            string conteudo = wc.DownloadString(novoEnderecoURL);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(conteudo);
+
+            return end;
+        }
     }
 }
